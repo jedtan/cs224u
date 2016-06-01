e5865cb07683bf1eac189bdad13561c44a37bf09
Tony Liu <tliu.auckland@gmail.com>
2016-06-01 01:04:07 -0700

    Added script to n chunks function

--- build_features.py
+++ build_features.py
@@ -1,57 +1,41 @@
-# Any of the files in glove.6B will work here:
-glove50_src = os.path.join(glove_home, 'glove.6B.50d.txt')
+from format_imsdb import format_script
+import math
+file_base = "imsdb_scripts/"
 
-# Creates a dict mapping strings (words) to GloVe vectors:
-GLOVE50 = utils.glove2dict(glove50_src)
+file_name = '{}{}'.format(file_base,"American Hustle.txt")
 
-def glove50vec(w):    
-    """Return `w`'s GloVe representation if available, else return 
-    a random vector."""
-    return GLOVE50.get(w, randvec(w, n=50))
 
-def build_dataset(
-        wordentail_data, 
-        vector_func=randvec, 
-        vector_combo_func=vec_concatenate): 
-    """
-    Parameters
-    ----------    
-    wordentail_data
-        The pickled dataset at `wordentail_data_filename`.
-    
-    vector_func : (default: `randvec`)
-        Any function mapping words in the vocab for `wordentail_data`
-        to vector representations
-        
-    vector_combo_func : (default: `vec_concatenate`)
-        Any function for combining two vectors into a new vector
-        of fixed dimensionality.
-        
-    Returns
-    -------
-    dataset : defaultdict
-        A map from split names ("train", "test", "disjoint_vocab_test")
-        into data instances:
-        
-        {'train': [(vec, [cls]), (vec, [cls]), ...],
-         'test':  [(vec, [cls]), (vec, [cls]), ...],
-         'disjoint_vocab_test': [(vec, [cls]), (vec, [cls]), ...]}
-    
-    """
-    # Load in the dataset:
-    vocab, splits = wordentail_data
-    # A mapping from words (as strings) to their vector
-    # representations, as determined by vector_func:
-    vectors = {w: vector_func(w) for w in vocab}
-    # Dataset in the format required by the neural network:
-    dataset = defaultdict(list)
-    for split, data in splits.items():
-        for clsname, word_pairs in data.items():
-            for w1, w2 in word_pairs:
-                # Use vector_combo_func to combine the word vectors for
-                # w1 and w2, as given by the vectors dictionary above,
-                # and pair it with the singleton array containing clsname:
-                item = [vector_combo_func(vectors[w1], vectors[w2]), 
-                        np.array([clsname])]
-                dataset[split].append(item)
-    return dataset
+scenes = format_script(file_name)
+
+# parameter to be tuned - segment length in number of minutes/pages
+
+
+# divide film into 30 minute chunks
+
+# Divide film by page numbers
+def script_to_n_chunks(scenes, num_segments = 4):
+	def get_script_by_page(scenes):
+		disaggregated_script = [event for events in scenes for event in events]
+		script_by_page = []
+		curr_events = []
+		for event in disaggregated_script:
+			if event[1] == 'page number':
+				script_by_page.append(curr_events)
+				curr_events = [] # don't include page number as event
+			else:
+				curr_events.append(event)
+		return script_by_page
+	# divide film into four chunks
+	# Returns generator
+	def get_n_chunks(l, n):
+		return [l[i:i+n] for i in xrange(0, len(l), n)]
+	def get_num_chunks(l, num_segments):
+		return math.ceil(len(l)/float(num_segments))
+	script_by_page = get_script_by_page(scenes)
+	num_chunks = get_num_chunks(script_by_page, num_segments)
+	temp = get_n_chunks(script_by_page, int(num_chunks))
+	chunks = []
+	# temp has four elements each with 38 elements
+	for chunk in temp:
+		chunks.append([event for page in chunk for event in page])
+	return chunks
